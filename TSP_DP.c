49720429e926ea43dc4c5283991085fa8c09d01a
Devansh Messon <46960431+zeph0yr@users.noreply.github.com>
2020-11-30 03:26:02 +0530

    Update TSP_DP.c

--- TSP_DP.c
+++ TSP_DP.c
@@ -1,30 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
 
 #ifndef ONLINE_JUDGE
-#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define freopen freopen("input.txt","r",stdin);freopen("out.txt","w",stdout);
 #else
 #define freopen //comment
 #endif
 
 #define int long long int
 int mat[60][60];
-int TSP_DP(int bitmask,int prevnode,int check,int nodes,int cost,int finalcost) //generating 2^n subsets
+int TSP_Bitmasking(int bitmask,int prevnode,int check,int nodes,int cost,int finalcost) //generating 2^n subsets
 {
   if(bitmask==check){return mat[prevnode][0];}
   for(int i=0;i<nodes;i++)
   {
     if((bitmask&(1<<i))==0) //if (i+1)th vertex is procressed for this particular path or not
     {
-      cost=mat[prevnode][i] + TSP_DP((bitmask |(1<<i)),i,check,nodes,cost,finalcost);//Recursion
-      if(finalcost>cost )finalcost=cost; //taking minimum of all costs
+       cost=mat[prevnode][i] + TSP_Bitmasking((bitmask |(1<<i)),i,check,nodes,cost,finalcost);//Recursion
+      if(finalcost>cost) finalcost=cost; //taking minimum of all costs
+      
     }
   }
   return finalcost;
 }
 int main()
 {
-  //freopen
+  freopen
   int nodes,edges,bitmask=1,prevnode=0,check,cost,finalcost=1e18,distance,a,b;
   scanf("%lld%lld",&nodes,&edges);
   check=(1<<nodes)-1;
@@ -47,14 +49,16 @@ int main()
        mat[b][a]=distance;
       }
   }
+  
+
   time_t start,end;
   start=clock();
-  int answer=TSP_DP(bitmask,prevnode,check,nodes,cost,finalcost);
-  printf("Cost of shortest path=%lld\n",answer);
+  int answer=TSP_Bitmasking(bitmask,prevnode,check,nodes,cost,finalcost);
+  printf("Cost of shortest path=%lld units\n",answer);
   end=clock();
 
   double num=end-start;
   double den=CLOCKS_PER_SEC;
   double time=num/den;
-  printf("\nTime=%f",time);
+  printf("\nTime=%f milliseconds",time*1000);
 }
